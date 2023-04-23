95d4e7c2de21a89ddef31d84db8f366ad1490367
Nishant2001 <89835549+Nishant2001@users.noreply.github.com>
2023-04-23 22:56:06 +0530

    Header function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,5 +3,42 @@ import React from "react";
 const Header = () => {
   return <div>Header</div>;
 };
+function Header() {
+  const classes = useStyles();
+  const { currency, setCurrency } = CryptoState();
+
+  const history = useHistory();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push(`/`)}
+              variant="h6"
+              className={classes.title}
+            >
+              Crypto Hunter
+            </Typography>
+            {/* <Button color="inherit">Login</Button> */}
+            <Select
+              variant="outlined"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={currency}
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
+
 
 export default Header;
