cf7880c66d9a455e63c0de2a13c376ac5fb6e449
unknown <adityakes321@gmail.com>
2023-05-02 12:31:36 +0530

    resolved dependency issues and minor bug fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import "react-alice-carousel/lib/alice-carousel.css";
 import CryptoContext from "./CryptoContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <CryptoContext>
-    <App />
-  </CryptoContext>
+ReactDOM.render(
+  <React.StrictMode>
+    <CryptoContext>
+      <App />
+    </CryptoContext>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
